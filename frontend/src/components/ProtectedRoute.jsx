7892aea03884e370c2c708901513f96bdaac4979
kmchatbot10 <wkimprensa@gmail.com>
2025-06-22 03:06:20 -0300

    limpeza: removendo arquivos ignorados corretamente

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-// components/ProtectedRoute.jsx
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const usuario = localStorage.getItem("usuario");
-  return usuario ? children : <Navigate to="/" />;
-}
--- start-kmzera.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start cmd /k "cd backend && npm install && node index.js"
-start cmd /k "cd whatsapp && npm install && node index.js"
-start cmd /k "cd frontend && npm install && npm run dev"
